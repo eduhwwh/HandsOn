fe955a4118b013d099d11c01d36cae5113566eb8
Maria Eduarda <vantev806@gmail.com>
2024-10-31 21:11:44 -0300

    Ultima atualizaçaõ

--- src/Js/app.js
+++ src/Js/app.js
@@ -18,11 +18,20 @@ function simular() {
     }
 
     var valorTotal = consumo * valorUnitario;
+    var Energisa = 1.70;
+
+    var Diferenca = Energisa * consumo;
+    var Economia = Diferenca - valorTotal;
 
     resultado.innerHTML = `
         <h2>Resultado da Simulação</h2>
         <p><strong>Papel:</strong> ${role.charAt(0).toUpperCase() + role.slice(1)}</p>
         <p><strong>Consumo:</strong> ${consumo.toFixed(2)} kWh</p>
         <p><strong>Valor Total:</strong> R$ ${valorTotal.toFixed(2)}</p>
+        <div class="result-box">
+            <h3>Economia Estimada</h3>
+            <p><strong>Valor que paga atualmente:</strong> R$ ${Diferenca.toFixed(2)}</p>
+            <p><strong>Passa a economizar:</strong> R$ ${Economia.toFixed(2)}</p>
+        </div>
     `;
 }
